3e51a7c575c02cfa65317b0ae6886a09172eed0d
ELhabib <elhabibbennani10@gmail.com>
2024-06-04 20:32:36 +0100

    Créer un projet et se connecter aux données Firebase

---
+++ my-authentication/src/App.js
@@ -0,0 +1,16 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import './App.css';
+import { Container } from 'react-bootstrap';
+
+
+function App() {
+  return (
+    <Container className='d-flex align-items-center justify-content-center'
+    style={{minHeight:"100vh"}}
+    >
+    
+    </Container>
+  );
+}
+
+export default App;
