28b0bbe2befb004f264f9467023de7d765e1b33a
ELhabib <elhabibbennani10@gmail.com>
2024-06-04 20:42:04 +0100

    Create react router

--- my-authentication/src/App.js
+++ my-authentication/src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import { Container } from 'react-bootstrap';
+import Signup from './components/Signup';
 
 
 function App() {
@@ -8,7 +9,17 @@ function App() {
     <Container className='d-flex align-items-center justify-content-center'
     style={{minHeight:"100vh"}}
     >
-    
+    <div className='w-100' style={{maxWidth: "400px"}} >
+      <Router>
+        <Routes>
+          <Route path='/signup' element={<Signup/>} />
+          <Route path='/login' element={<Login/>} />
+          <Route path='/forgot-password' element={<ForgotPassword/>} />
+          <Route path='/update-profile' element={<UpdateProfile/>} />
+          <Route path='/' element={<Dashboard/>} />
+        </Routes>
+      </Router>
+    </div>
     </Container>
   );
 }
